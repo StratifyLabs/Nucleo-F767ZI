dd2b32f5b14323758550d1c280d30d8fe16a33ab
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-04-09 11:55:57 -0600

    Version 0.9

--- src/config.h
+++ src/config.h
@@ -29,7 +29,7 @@ limitations under the License.
 #define SOS_BOARD_SYSTEM_CLOCK 216000000
 #define SOS_BOARD_SYSTEM_MEMORY_SIZE (8192*3)
 #define SOS_BOARD_ID "-L7SFP-AgTrNQp-8Mrk1"
-#define SOS_BOARD_VERSION "0.8"
+#define SOS_BOARD_VERSION "0.9"
 #define SOS_BOARD_NAME "Nucle-F767ZI"
 
 #define SOS_BOARD_USB_RX_BUFFER_SIZE 512
