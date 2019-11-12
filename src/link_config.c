57e42e69d70575d3a23ccf474fef562f5cd2f5ce
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-11-11 21:50:56 -0700

    API updates link and flash mv

--- src/link_config.c
+++ src/link_config.c
@@ -71,6 +71,9 @@ link_transport_driver_t link_transport = {
 	.close = sos_link_transport_usb_close,
 	.wait = sos_link_transport_usb_wait,
 	.flush = sos_link_transport_usb_flush,
+	.o_flags = 0, //no checksums -- checksums already happen over USB
+	.transport_read = link1_transport_slaveread,
+	.transport_write = link1_transport_slavewrite,
 	.timeout = 500
 };
 
--- src/sl_config.h
+++ src/sl_config.h
@@ -9,6 +9,7 @@
 #define SL_CONFIG_VERSION_STRING "0.13"
 #define SL_CONFIG_VERSION_BCD 0x0D
 #define SL_CONFIG_DOCUMENT_ID "8fgoFM3GV4Kos65AegZT"
+#define SL_CONFIG_TEAM_ID ""
 #define SL_CONFIG_NAME "Nucleo-F767ZI"
 #define SL_CONFIG_TYPE "os"
 #define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
